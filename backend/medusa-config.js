446a72a90ab678f837c88bc9b4341b630b54a8dc
Rasmus Puls <rasmus@triggerz.net>
2024-07-19 12:04:49 +0200

    removed cloudinary file storage

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -33,24 +33,11 @@ const DATABASE_URL =
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
-const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
-const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
-const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
-const cloudinaryConfigured = CLOUDINARY_CLOUD_NAME && CLOUDINARY_API_KEY && CLOUDINARY_API_SECRET;
+
 
 const ADMIN_APP_PORT = process.env.PORT || 7001;
 
-const fileServicePlugin = cloudinaryConfigured
-  ? {
-    resolve: `medusa-file-cloudinary`,
-    options: {
-      cloud_name: CLOUDINARY_CLOUD_NAME,
-      api_key: CLOUDINARY_API_KEY,
-      api_secret: CLOUDINARY_API_SECRET,
-      secure: true,
-    },
-  }
-  : {
+const fileServicePlugin = {
     resolve: `@medusajs/file-local`,
     options: {
       upload_dir: "uploads",
